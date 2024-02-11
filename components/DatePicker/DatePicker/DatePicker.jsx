95fa749597d0969f9ade73e7e332aa87a57cb63e
leo.liu <leo.liu@langlive.com>
2024-02-12 01:20:22 +0800

    add vscode setting, adjust DatePicker logic

--- components/DatePicker/DatePicker/DatePicker.jsx
+++ components/DatePicker/DatePicker/DatePicker.jsx
@@ -1,113 +1,116 @@
-"use client";
+'use client'
 
-import { useState } from 'react';
-import dayjs from 'dayjs';
-import 'dayjs/locale/en'; // 引入 dayjs 英文語言包
-import 'dayjs/locale/zh-tw'; // 引入 dayjs 繁體中文語言包
-dayjs.locale('zh-tw'); // 設定 dayjs 使用繁體中文語言
+import { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
+import 'dayjs/locale/en'
+import 'dayjs/locale/zh-tw'
+dayjs.locale('zh-tw')
 import Button from '@/components/Button/Button'
-const DatePicker = ({ buttonClass = 'w-[44px] h-[44px] hover:bg-[#e6e6e6]', }) => {
+
+const DatePicker = ({ buttonClass = 'w-[44px] h-[44px] hover:bg-[#e6e6e6]', onGetTimeCallback = () => {} }) => {
   const isBetween = require('dayjs/plugin/isBetween')
   dayjs.extend(isBetween)
-  const isSameOrAfter = require('dayjs/plugin/isSameOrAfter')
-  const [startDate, setStartDate] = useState(null);
-  const [endDate, setEndDate] = useState(null);
-  const [currentMonth, setCurrentMonth] = useState(dayjs());
+  const [startDate, setStartDate] = useState(null)
+  const [endDate, setEndDate] = useState(null)
+  const [currentMonth, setCurrentMonth] = useState(dayjs())
+  const [calendarDates, setCalendarDates] = useState([])
 
-  const handleDateClick =({ showDate, date}) => () =>  {
-    if (showDate) {
-      if (!startDate || date.isBefore(startDate)) {
-        setStartDate(date);
-        setEndDate(null);
-      } else if (date.isSame(startDate) || date.isAfter(startDate)) {
-        setEndDate(date);
-      }
-      //  else {
-      //   setEndDate(startDate);
-      //   setStartDate(date);
-      // }
-  
+  const handleDateClick = date => () => {
+    if ((startDate && endDate) || !startDate || date.isBefore(startDate)) {
+      setStartDate(date)
+      setEndDate(null)
+    } else if (date.isSame(startDate) || date.isAfter(startDate)) {
+      setEndDate(date)
     }
-  };
+  }
 
-  const handleMonthChange =  (newDate) => () => {
-    setCurrentMonth(newDate);
-  };
+  const handleMonthChange = newDate => () => {
+    setCurrentMonth(newDate)
+  }
 
-  const isNonCurrentMonth = (date) => date && !date.isSame(currentMonth, 'month');
+  useEffect(() => {
+    const firstDay = currentMonth.startOf('month').startOf('week')
+    const lastDay = currentMonth.endOf('month').endOf('week')
 
-  const isDisabled = (date) => isNonCurrentMonth(date);
-
-  const getCalendarDates = () => {
-    const firstDay = currentMonth.startOf('month').startOf('week');
-    const lastDay = currentMonth.endOf('month').endOf('week');
-
-    const calendarDates = [];
-    let currentDate = firstDay;
+    const calendarDates = []
+    let currentDate = firstDay
 
     while (currentDate.isBefore(lastDay)) {
-      if (!calendarDates.some(date => date && date.isSame(currentDate, 'day'))) {
-        calendarDates.push(currentDate);
-      }
-      currentDate = currentDate.add(1, 'day');
+      calendarDates.push(currentDate)
+      currentDate = currentDate.add(1, 'day')
     }
 
-    return calendarDates.reduce((rows, currentDay, index) => {
-      if (index % 7 === 0) rows.push([]);
-      rows[rows.length - 1].push(currentDay);
-      return rows;
-    }, []);
-  };
-  console.log('startDate', startDate, dayjs(startDate))
-  console.log('endDate', endDate)
+    const formattedCalendarDates = calendarDates.reduce((rows, currentDay, index) => {
+      if (index % 7 === 0) rows.push([])
+      rows[rows.length - 1].push(currentDay)
+      return rows
+    }, [])
+
+    setCalendarDates(formattedCalendarDates)
+  }, [currentMonth])
+
+  useEffect(() => {
+    if (startDate && endDate) {
+      onGetTimeCallback({ startDate, endDate })
+    }
+  }, [startDate, endDate])
+
   return (
-    <div className='w-[350px] h-[240px] text-[16px]' >
-      <div className="h-[44px] mb-[16px] flex items-center justify-between mb-4">
-        <Button className={buttonClass} onClick={handleMonthChange(currentMonth.subtract(1, 'month'))}>{'<'}</Button>
+    <div className='w-[350px] h-[240px] text-[16px]'>
+      <div className='h-[44px] mb-[16px] flex items-center justify-between mb-4'>
+        <Button className={buttonClass} onClick={handleMonthChange(currentMonth.subtract(1, 'month'))}>
+          {'<'}
+        </Button>
         <span>{currentMonth.format('MMMM YYYY')}</span>
-        <Button className={buttonClass} onClick={handleMonthChange(currentMonth.add(1, 'month'))}>{'>'}</Button>
+        <Button className={buttonClass} onClick={handleMonthChange(currentMonth.add(1, 'month'))}>
+          {'>'}
+        </Button>
       </div>
       <table>
         <thead>
           <tr>
             {[
-              { key:'Sun', title: '週日'},
-              { key:'Mon', title: '週一'},
-              { key:'Tue', title: '週二'},
-              { key:'Wed', title: '週三'},
-              { key:'Thu', title: '週四'},
-              { key:'Fri', title: '週五'},
-              { key:'Sat', title: '週六'},
-              ].map(({ key, title}) => (
+              { key: 'Sun', title: '週日' },
+              { key: 'Mon', title: '週一' },
+              { key: 'Tue', title: '週二' },
+              { key: 'Wed', title: '週三' },
+              { key: 'Thu', title: '週四' },
+              { key: 'Fri', title: '週五' },
+              { key: 'Sat', title: '週六' },
+            ].map(({ key, title }) => (
               <th key={key}>{title}</th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {getCalendarDates().map((week, weekIndex) => (
+          {calendarDates.map((week, weekIndex) => (
             <tr key={weekIndex}>
-              {week.map((day) => {
+              {week.map(day => {
                 return (
-                <td
-                  key={day.format('YYYY-MM-DD')}
-                  onClick={handleDateClick({ showDate: !isNonCurrentMonth(day), date: day })}
-                  className={`w-[50px] h-[36px] text-center ${isDisabled(day) ? 'cursor-not-allowed text-[#757575]' : 'cursor-pointer'} 
-                                ${day.isSame(startDate) || (startDate && endDate &&  dayjs(day).isBetween(startDate, endDate, 'day', '[]'))  ? 'bg-[#006edc] text-white' : ''}
+                  <td
+                    key={day.format('YYYY-MM-DD')}
+                    onClick={handleDateClick(day)}
+                    className={`w-[50px] h-[36px] text-center cursor-pointer
+                                ${
+                                  day.isSame(startDate) ||
+                                  (startDate && endDate && dayjs(day).isBetween(startDate, endDate, 'day', '[]'))
+                                    ? 'bg-[#006edc] text-white'
+                                    : ''
+                                }
                                 ${day.isSame(dayjs(), 'day') ? 'bg-[#ffff76]' : ''}
                                 hover:bg-[#e6e6e6]`}
-                >
-                  <div className='inline w-[20px]'>{day.format('D')}</div>
-                  <div className='inline text-center w-[20px]'>日</div>
-                </td>
-              )
+                  >
+                    <div className='inline w-[20px]'>{day.format('D')}</div>
+                    <div className='inline text-center w-[20px]'>日</div>
+                  </td>
+                )
               })}
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
-export default DatePicker;
-
+export default DatePicker
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,17 @@
-
-import DatePicker from "@/components/DatePicker/DatePicker/DatePicker";
+'use client'
+import DatePicker from '@/components/DatePicker/DatePicker/DatePicker'
+import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export default function Home() {
+  const [{ startDate, endDate }, setTimeData] = useState({})
   return (
-    <main className=" flex min-h-screen flex-col items-center justify-between p-24">
-      <DatePicker />
+    <main className=' flex min-h-screen flex-col items-center justify-between p-24'>
+      <DatePicker onGetTimeCallback={setTimeData} />
+      startDate: {startDate && dayjs(startDate).format()}
+      <br />
+      endDate: {endDate && dayjs(endDate).format()}
+      <br />
     </main>
-  );
+  )
 }
