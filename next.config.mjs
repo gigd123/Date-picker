a30e732e7a14ec658cf4a24c873abfd4d588f741
leo.liu <leo.liu@langlive.com>
2024-02-15 01:41:06 +0800

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = ''
+let basePath = ''
 
-export default nextConfig;
+if (isGithubActions) {
+  // 去掉 `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+const nextConfig = {
+  output: 'export',
+  basePath,
+  assetPrefix,
+}
+
+export default nextConfig
